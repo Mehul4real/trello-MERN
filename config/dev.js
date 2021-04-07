a8a8a4f5461dc9d1f4afcfaf5161d855c5b3e4f2
Mehul4real <mehulsaxena146@gmail.com>
2021-04-07 23:57:08 +0530

    Full Project

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Mehul:jK0G2wWG2XFJ0Tnx@cluster0.7ulz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"rs6g532sef5s6ecsf9f653bt3gdr96g54a3wd1aw5d3adef8451awd"
+}
